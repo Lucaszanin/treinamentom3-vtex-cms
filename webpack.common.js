892a586722d3fc27106035da75808bb391849092
Heraldo Ecard Roque Filho <heraldo@digitalm3.com.br>
2020-03-07 11:17:13 -0300

    adicionado alias para as principais pastas do JS, e js config para o vscode não perder o caminho.

--- webpack.common.js
+++ webpack.common.js
@@ -29,6 +29,14 @@ module.exports = {
 			}
 		]
 	},
+	resolve: {
+		alias: {
+			Helpers: path.resolve(__dirname, "src/arquivos/js/helpers"),
+			Lib: path.resolve(__dirname, "src/arquivos/js/lib"),
+			Config: path.resolve(__dirname, "src/arquivos/js/config"),
+			App: path.resolve(__dirname, "src/arquivos/js/app")
+		}
+	},
 	plugins: [
 		new webpack.ProvidePlugin({
 			$: "jquery",
