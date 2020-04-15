f80994e7df1435df8a42038155cbd501a85ca1e9
Heraldo Ecard Roque Filho <me@heraldo.tech>
2020-04-15 13:42:55 -0300

    Adicionado o polyfill dinamico

--- src/arquivos/js/main.js
+++ src/arquivos/js/main.js
@@ -3,12 +3,6 @@ import "lazysizes/plugins/noscript/ls.noscript";
 import "slick-carousel";
 import app from "./app";
 
-if (/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
-	document.write(
-		'<script src="https://cdn.jsdelivr.net/npm/url-polyfill@1.1.8/url-polyfill.min.js"></script>'
-	);
-}
-
 window.lazySizesConfig = {
 	addClasses: true
 };
