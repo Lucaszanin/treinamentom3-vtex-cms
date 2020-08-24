79c0294f7ef8e83e506afb8e230bf919347db841
Davi P. Guimarães <davi@digitalm3.com.br>
2020-08-24 09:15:40 -0300

    Refactor module ModuloSkus.js to class

--- src/arquivos/js/lib/ComponentesProduto/ModuloSkus.js
+++ src/arquivos/js/lib/ComponentesProduto/ModuloSkus.js
@@ -1,73 +1,50 @@
 import { Modulo } from "./Modulo";
 import { CHANGE_SKU, SKU_REF } from "./EventType";
-/**
- * modulo de seleção dos skus
- * Permite escolher o Sku desejado
- */
-export var ModuloSkus = function(
-	skuJson,
-	elemento = ".skuSelection",
-	componentStore
-) {
-	Modulo.call(this, elemento, componentStore);
-	sessionStorage.removeItem("sku-selecionado");
-	this._skuJson = skuJson;
-	var skuReferencial;
+
+export default class ModuloSkus extends Modulo{
+	constructor( skuJson, elemento = ".skuSelection", componentStore ) {
+		super(elemento, componentStore);
+		sessionStorage.removeItem("sku-selecionado");
+		this._skuJson = skuJson;
+	}
 
 	/**
 	 * Atualiza os valores no html que foi criado
 	 * @return {object} this
 	 */
-	this.atualizar = function() {
+	atualizar() {
 		this.desenhar();
 		return this;
 	};
+
 	/**
 	 * Escolhe os primeiros skus de cada variação
 	 * @return {object} this
 	 */
-	this.setDefauls = function() {
-		return this;
-	};
-	/**
-	 * Cria e insere o html com as variações dos skus
-	 * @param  {Object} mapaEspecificacoes Mapa das especificações do produto
-	 * @return {object} this
-	 */
-	this.desenhar = function() {
-		return this;
-	};
-	/**
-	 * Configura os eventos de atualizacao
-	 * @return {object} this
-	 */
-	this.configurar = function() {
+	setDefauls() {
 		return this;
 	};
 
-	this.escolherSku = function(sku) {
+	escolherSku(sku) {
 		if (sku) {
-			componentStore.events.publish(CHANGE_SKU, sku);
-			componentStore.commit("setSelectedSku", sku);
+			this._store.events.publish(CHANGE_SKU, sku);
+			this._store.commit("setSelectedSku", sku);
 		} else {
 			console.warn("Não conseguimos identificar o sku correspondente");
 			// console.warn(especificacoesDoSku);
 		}
 	};
 
-	this.escolherSkuReferencia = function(sku) {
-		if (typeof skuReferencial === "undefined") {
-			skuReferencial = sku;
+	escolherSkuReferencia(sku) {
+		if (typeof this._skuReferencial === "undefined") {
+			this._skuReferencial = sku;
 			/**
 			 * para capturar o evento
 			 * $(document).on( 'change-sku' , function(event, novoSku){} );
 			 */
 
 			sessionStorage.setItem("sku-referencial", JSON.stringify(sku));
-			componentStore.events.publish(SKU_REF, sku);
+			this._store.events.publish(SKU_REF, sku);
 		}
 	};
-};
-// subclasse extende superclasse
-ModuloSkus.prototype = Object.create(Modulo.prototype);
-ModuloSkus.prototype.constructor = ModuloSkus;
+}
