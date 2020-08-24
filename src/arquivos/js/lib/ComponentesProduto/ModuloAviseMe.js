3de2540ebf408d8308aaa713aeee0e87518ac272
Davi P. Guimarães <davi@digitalm3.com.br>
2020-08-24 09:15:40 -0300

    Refactor module ModuloAviseMe.js to class

--- src/arquivos/js/lib/ComponentesProduto/ModuloAviseMe.js
+++ src/arquivos/js/lib/ComponentesProduto/ModuloAviseMe.js
@@ -1,48 +1,49 @@
 import { Modulo } from "./Modulo";
 import { CHANGE_SKU, SKU_REF } from "./EventType";
-/**
- * Modulo de quantidade
- * Permite escolher a quantidade de um sku
- */
-export var ModuloAviseMe = function (
-	elemento = ".avise-me-container:first-child",
-	componentStore
-) {
-	Modulo.call(this, elemento, componentStore);
-	var _this = this;
 
-	this._opcoes = {
-		titulo:
-			'Avise-me quando o produto <span id="avise-me-produto-nome"></span> estiver disponível',
-		subtitulo: "",
-		fechar: "&#215;",
-		placeholderNome: "Digite seu nome...",
-		placeholderEmail: "Digite seu e-mail...",
-		btnSubmit: "Enviar",
-		msgSucesso: "Cadastrado com sucesso!",
-		msgErro: "Ocorreu algum erro. Tente novamente mais tarde.",
-	};
+
+
+export default class ModuloAviseMe extends Modulo{
+
+	constructor(elemento= ".avise-me-container:first-child", componentStore) {
+		super(elemento, componentStore);
+		this.opcoes({
+			titulo:
+				'Avise-me quando o produto <span id="avise-me-produto-nome"></span> estiver disponível',
+			subtitulo: "",
+			fechar: "&#215;",
+			placeholderNome: "Digite seu nome...",
+			placeholderEmail: "Digite seu e-mail...",
+			btnSubmit: "Enviar",
+			msgSucesso: "Cadastrado com sucesso!",
+			msgErro: "Ocorreu algum erro. Tente novamente mais tarde.",
+		});
+	}
+
 	/**
 	 * Configura os eventos js que serão diparados pelo html do desenhar()
 	 * @return {object} this
 	 */
-	this.configurar = function (opcoes) {
-		this.opcoes($.extend({}, this._opcoes, opcoes));
-		this._store.events.subscribe(SKU_REF, function (event, sku) {
-			_this.atualizar(sku);
+	configurar (opcoes) {
+		super.configurar(opcoes);
+
+		this._store.events.subscribe(SKU_REF, (event, sku) =>{
+			this.atualizar(sku);
 		});
-		this._store.events.subscribe(CHANGE_SKU, function (event, sku) {
-			_this.atualizar(sku);
+		this._store.events.subscribe(CHANGE_SKU, (event, sku) =>{
+			this.atualizar(sku);
 		});
 		return this;
-	};
+	}
+
+
 	/**
 	 * Atualiza a quantidade de acordo com o novo sku
 	 * @param  {Event} event evento que disparou atualização
 	 * @param  {Object} novoSku objeto do sku selecionado
 	 * @return {Object} this
 	 */
-	this.atualizar = function (novoSku) {
+	atualizar (novoSku) {
 		if (!novoSku) {
 			novoSku = {
 				available: false,
@@ -62,87 +63,53 @@ export var ModuloAviseMe = function (
 		this.elemento().toggle(!novoSku.available);
 
 		return this;
-	};
+	}
+
+
 	/**
 	 * Cria e insere o html com as variações dos skus
 	 * @return {object} this
 	 */
-	this.desenhar = function () {
+	desenhar() {
 		if ($(".form-avise-me").length == 0) {
-			var container = $("<div />")
-				.addClass("avise-me-container")
-				.appendTo(this.elemento());
-			var fieldset = $("<fieldset />")
-				.addClass("form-avise-me")
-				.appendTo(container);
-			$("<a />")
-				.attr({
-					class: "close",
-					text: this.opcoes().fechar,
-				})
-				.click(
-					function () {
-						this.elemento().hide();
-					}.bind(this)
-				)
-				.appendTo(fieldset);
-			$("<h2 />", {
-				html: this.opcoes().titulo,
-			}).appendTo(fieldset);
+			let _html = `<div class="avise-me-container">
+				<fieldset class="form-avise-me">
+					<a class="close" >${this.opcoes().fechar}</a>
+					<h2> ${this.opcoes().titulo} </h2>
+					${(this.opcoes().subtitulo)?`<h3> ${this.opcoes().subtitulo} </h3>`:''}
+					<div class="avise-me-wrapper">
+						<label >
+							<span>${this.opcoes().placeholderNome}</span>
+							<input name="notifymeClientName" type="text" id="nome-avise-me"
+							placeholder="Ex.: João da silva">
+						</label>
+						<label >
+							<span>${this.opcoes().placeholderEmail}</span>
+							<input name="notifymeClientEmail" type="text" id="email-avise-me"
+							placeholder="Ex.: joao.da.silva@mail.com">
+						</label>
+					</div>
+					<input name="notifymeIdSku" type="hidden" id="sku-avise-me" class="notifyme-skuid" value="0">
+					<input name="notifymeButtonOK" type="button" id="enviar-avise-me" class="btn-enviar" value="${this.opcoes().btnSubmit}">
+					<p class="status"></p>
+				</fieldset>
+			</div>`;
 
-			if (this.opcoes().subtitulo) {
-				$("<h3 />", {
-					html: this.opcoes().subtitulo,
-				}).appendTo(fieldset);
-			}
 
-			var fieldsWrapper = $("<div/>", {
-				class: "avise-me-wrapper",
-			}).appendTo(fieldset);
+			var $aviseme = $(_html).appendTo(this.elemento());
 
-			$("<input />", {
-				name: "notifymeClientName",
-				type: "text",
-				id: "nome-avise-me",
-				size: "20",
-				placeholder: this.opcoes().placeholderNome,
-			}).appendTo(fieldsWrapper);
-			$("<input />", {
-				name: "notifymeClientEmail",
-				type: "text",
-				id: "email-avise-me",
-				size: "20",
-				placeholder: this.opcoes().placeholderEmail,
-			}).appendTo(fieldsWrapper);
+			$aviseme.on('click','.close',()=>{this.elemento().hide();})
+			$aviseme.on('click','#enviar-avise-me',this.enviar.bind(this))
 
-			$("<input />", {
-				name: "notifymeIdSku",
-				type: "hidden",
-				id: "sku-avise-me",
-				class: "notifyme-skuid",
-				value: 0,
-			}).appendTo(fieldset);
-
-			$("<input />", {
-				name: "notifymeButtonOK",
-				type: "button",
-				id: "enviar-avise-me",
-				class: "btn-enviar",
-				value: this.opcoes().btnSubmit,
-			})
-				.click(this.enviar.bind(this))
-				.appendTo(fieldset);
-			$("<p />", {
-				class: "status",
-			}).appendTo(fieldset);
 		}
 		this.atualizar(this._store.state.selectedSku);
 		return this;
-	};
+	}
+
 	/**
 	 * Funçõa que envia registra a solicitação de "avise-me"
 	 */
-	this.enviar = function () {
+	enviar () {
 		var aviseme = this.elemento().find(".form-avise-me");
 		if (!this.validar(aviseme)) {
 			return false;
@@ -171,7 +138,7 @@ export var ModuloAviseMe = function (
 	 * Função para validar os dados do formulário
 	 * @param {jQueryElement} aviseme
 	 */
-	this.validar = function validar(aviseme) {
+	validar(aviseme) {
 		var nome = aviseme.find("#nome-avise-me");
 		var email = aviseme.find("#email-avise-me");
 		var filter = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
@@ -187,7 +154,5 @@ export var ModuloAviseMe = function (
 		}
 		return true;
 	};
-};
-// subclasse extende superclasse
-ModuloAviseMe.prototype = Object.create(Modulo.prototype);
-ModuloAviseMe.prototype.constructor = ModuloAviseMe;
+
+}
