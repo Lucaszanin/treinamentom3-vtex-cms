b9249014e5821164aa2d18cca6a7fa3adb958624
Heraldo Ecard Roque Filho <me@heraldo.tech>
2020-09-19 14:09:03 -0300

    atualizado o template com as master e validacao para conteplace holder repetidos

--- dev/vtexEngine/VtexEngine.js
+++ dev/vtexEngine/VtexEngine.js
@@ -10,6 +10,28 @@ class VtexEngine {
 			this.metaData,
 			this.files.prateleiras
 		);
+		this.validateMetaData();
+	}
+
+	validateMetaData() {
+		this.metaData.pages.forEach((pageData) => {
+			let ids = {};
+			let dups = [];
+			pageData.data.contentPlaceHolders.forEach((val) => {
+				if (ids[val.id]) {
+					dups.push(val);
+				} else {
+					ids[val.id] = true;
+				}
+			});
+
+			if (dups.length > 0) {
+				console.log(
+					`\n\n   \x1b[31m O ID: "${dups[0].id}" foi encontrado multiplas vezes no template: "${pageData.template}" \n\n`
+				);
+				throw null;
+			}
+		});
 	}
 
 	process(fileContent, basename, transformCTX) {
