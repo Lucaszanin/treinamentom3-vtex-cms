d00a74aa65fe8ffc94a050850ddf8f2ed6417f84
Heraldo Ecard Roque Filho <me@heraldo.tech>
2020-11-11 17:10:54 -0300

    correção de flag duplicada

--- src/arquivos/js/app/components/Prateleira/Prateleira.js
+++ src/arquivos/js/app/components/Prateleira/Prateleira.js
@@ -22,7 +22,7 @@ export default class Prateleira {
 			) {
 				this.createFlagDesconto($(el), percentualDeDesconto);
 			}
-			$(this).addClass("flagDeDesconto");
+			$(el).addClass("flagDeDesconto");
 		});
 	}
 	precoParcelado() {
