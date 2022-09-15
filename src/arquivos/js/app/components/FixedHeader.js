284851b4c0f61af66a9111fc7138347c14bdd835
unknown <lukinhas.so.lokura@hotmail.com>
2022-09-15 11:25:08 -0300

    feat: Deixando header fixo com scroll

--- src/arquivos/js/app/components/FixedHeader.js
+++ src/arquivos/js/app/components/FixedHeader.js
@@ -1,45 +1,28 @@
-import { isSmallerThen768 } from "Helpers/MediasMatch";
+import { isSmallerThen991 } from "Helpers/MediasMatch";
 
 export default class FixedHeader {
 	constructor() {
-		if (!isSmallerThen768) {
-			this.fixedHeader();
+		if (!isSmallerThen991) {
+			this.selectors();
+			this.events();
 		}
 	}
+	
+	selectors(){
+		this.pageHeader = $('.page-header');
+	}
 
-	fixedHeader() {
-		var lastScroll = 0;
-		var timer, scrollTop, headerHeight;
-		var element = $("header.header");
+	events(){
+		$(document).scroll(this.fixHeader.bind(this))
+	}
 
-		$(document).scroll(function (e) {
-			headerHeight = element.height();
-			scrollTop = $(document).scrollTop();
-
-			if (scrollTop > 1) {
-				$("body").css("padding-top", headerHeight);
-				element.addClass("fixed");
-
-				if (timer) {
-					window.clearTimeout(timer);
-				}
-
-				timer = window.setTimeout(function () {
-					if (
-						scrollTop > lastScroll &&
-						scrollTop > element.height() + 20
-					) {
-						element.addClass("fixed-hide");
-					} else {
-						element.removeClass("fixed-hide");
-					}
-
-					lastScroll = $(document).scrollTop();
-				}, 50);
-			} else {
-				element.removeClass("fixed");
-				$("body").css("padding-top", 0);
-			}
-		});
+	fixHeader(){
+		const topScroll = $(document).scrollTop();
+		console.log(topScroll);
+		if(topScroll > 200){
+			this.pageHeader.addClass("is-fixed")
+		}else {
+			this.pageHeader.removeClass("is-fixed")
+		}
 	}
 }
